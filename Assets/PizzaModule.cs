16b8d56a29cf12b5246312d27a398b7a68a68bdd
John van Terheijden <john@fastguide.eu>
2018-05-02 15:45:48 +0200

    Add all recipes and prefab ingredients.

--- Assets/PizzaModule.cs
+++ Assets/PizzaModule.cs
@@ -8,26 +8,35 @@ using Rnd = UnityEngine.Random;
 public class PizzaModule : MonoBehaviour
 {
     public KMSelectable Module;
+    public KMSelectable Order;
     public KMSelectable[] BeltNodes;
     public KMSelectable[] PlateNodes;
     public GameObject[] Ingredients;
     public enum Ingredient
     {
-        Artichokes, Bacon, Basil, BbqSauce, BellPeppers, BlackOlives, Cheddar, GrilledChickenBreast, GroundBeef, Ham, ItalianSausage, Jalapeño, Mozzarella, Mushrooms, Mussels, Olives, Onions, Pepperoni, Pineapple, RedOnions, Scampi, Tomatoes, Tuna
+        Artichokes, Bacon, Basil, BbqSauce, BellPeppers, BlackOlives, Cheddar, GrilledChickenBreast, Ham, ItalianSausage, Jalapeño, Mozzarella, Mushrooms, Mussels, Pepperoni, Pineapple, RedOnions, Scampi, Tomatoes, Tuna
     }
     public enum Pizza
     {
         Margherita, BbqChicken, BuffaloChicken, Adventure, IScream, FruttiDiMare, Hawaii, MeatLovers, Veggie, BaconCheddar, TunaDelight, QuattroStagioni
     }
+    public enum Customer
+    {
+        Bob, Carlo, Clair, Frank, Frédérique, Ingrid, Melissa, Natasha, Sandy, Sigmund, Tyrone
+    };
 
     private Dictionary<Ingredient, string> _ingredientNames;
     private Dictionary<Pizza, PizzaRecipe> _pizzaRecipes;
     private List<Item> _itemsOnBelt;
     private List<Item> _itemsOnPlate;
-    private List<Customer> _customers;
+    private Queue<Ingredient> _queuedIngredients = new Queue<Ingredient>();
+    private Pizza _pizza;
+    private Customer _customer;
 
     void Start()
     {
+        Order.transform.Find("Text").GetComponent<TextMesh>().text = "";
+
         _ingredientNames = new Dictionary<Ingredient, string>()
         {
             { Ingredient.Artichokes, "Artichokes" },
@@ -38,15 +47,12 @@ public class PizzaModule : MonoBehaviour
             { Ingredient.BlackOlives, "Black olives" },
             { Ingredient.Cheddar, "Cheddar" },
             { Ingredient.GrilledChickenBreast, "Grilled chicken breast" },
-            { Ingredient.GroundBeef, "Ground beef" },
             { Ingredient.Ham, "Ham" },
             { Ingredient.ItalianSausage, "Italian sausage" },
             { Ingredient.Jalapeño, "Jalapeño" },
             { Ingredient.Mozzarella, "Mozzarella" },
             { Ingredient.Mushrooms, "Mushrooms" },
             { Ingredient.Mussels, "Mussels" },
-            { Ingredient.Olives, "Olives" },
-            { Ingredient.Onions, "Onions" },
             { Ingredient.Pepperoni, "Pepperoni" },
             { Ingredient.Pineapple, "Pineapple" },
             { Ingredient.RedOnions, "Red onions" },
@@ -63,13 +69,13 @@ public class PizzaModule : MonoBehaviour
                 Ingredient.BbqSauce, Ingredient.GrilledChickenBreast, Ingredient.RedOnions, Ingredient.Bacon, Ingredient.Mozzarella
             } } },
             { Pizza.BuffaloChicken, new PizzaRecipe() { Name = "Buffalo Chicken", Ingredients = new List<Ingredient>() {
-                Ingredient.BbqSauce, Ingredient.GrilledChickenBreast, Ingredient.RedOnions, Ingredient.Cheddar, Ingredient.Mozzarella
+                Ingredient.BbqSauce, Ingredient.GrilledChickenBreast, Ingredient.RedOnions, Ingredient.Cheddar
             } } },
             { Pizza.Adventure, new PizzaRecipe() { Name = "Adventure", Ingredients = new List<Ingredient>() {
-                Ingredient.Pepperoni, Ingredient.Ham, Ingredient.BellPeppers, Ingredient.Scampi, Ingredient.Jalapeño, Ingredient.Mozzarella
+                Ingredient.Pepperoni, Ingredient.ItalianSausage, Ingredient.Artichokes, Ingredient.Scampi, Ingredient.Jalapeño, Ingredient.Cheddar
             } } },
             { Pizza.IScream, new PizzaRecipe() { Name = "I Scream", Ingredients = new List<Ingredient>() {
-                Ingredient.Pepperoni, Ingredient.Ham, Ingredient.BellPeppers, Ingredient.Mussels, Ingredient.Jalapeño, Ingredient.Jalapeño, Ingredient.Mozzarella
+                Ingredient.Pepperoni, Ingredient.GrilledChickenBreast, Ingredient.BellPeppers, Ingredient.Mussels, Ingredient.Jalapeño, Ingredient.Jalapeño
             } } },
             { Pizza.FruttiDiMare, new PizzaRecipe() { Name = "Frutti di Mare", Ingredients = new List<Ingredient>() {
                 Ingredient.Tuna, Ingredient.Scampi, Ingredient.Mussels, Ingredient.BlackOlives
@@ -78,13 +84,13 @@ public class PizzaModule : MonoBehaviour
                 Ingredient.Ham, Ingredient.Bacon, Ingredient.Pineapple, Ingredient.Mozzarella
             } } },
             { Pizza.MeatLovers, new PizzaRecipe() { Name = "Meat Lovers", Ingredients = new List<Ingredient>() {
-                Ingredient.Pepperoni, Ingredient.Ham, Ingredient.ItalianSausage, Ingredient.GroundBeef, Ingredient.Mozzarella
+                Ingredient.Pepperoni, Ingredient.Ham, Ingredient.ItalianSausage, Ingredient.Mozzarella
             } } },
             { Pizza.Veggie, new PizzaRecipe() { Name = "Veggie", Ingredients = new List<Ingredient>() {
-                Ingredient.Mushrooms, Ingredient.BellPeppers, Ingredient.Onions, Ingredient.BlackOlives, Ingredient.Tomatoes, Ingredient.Mozzarella
+                Ingredient.Mushrooms, Ingredient.BellPeppers, Ingredient.Artichokes, Ingredient.BlackOlives, Ingredient.Tomatoes
             } } },
             { Pizza.BaconCheddar, new PizzaRecipe() { Name = "Bacon Cheddar", Ingredients = new List<Ingredient>() {
-                Ingredient.GroundBeef, Ingredient.Bacon, Ingredient.Cheddar, Ingredient.Mozzarella
+                Ingredient.ItalianSausage, Ingredient.Bacon, Ingredient.Cheddar, Ingredient.Cheddar
             } } },
             { Pizza.TunaDelight, new PizzaRecipe() { Name = "Tuna Delight", Ingredients = new List<Ingredient>() {
                 Ingredient.Tuna, Ingredient.RedOnions, Ingredient.BlackOlives, Ingredient.Mozzarella
@@ -94,33 +100,10 @@ public class PizzaModule : MonoBehaviour
             } } },
         };
 
-        _customers = new List<Customer>() {
-            new Customer { Name = "Bob", Description = "<b>Bob</b> is a vegan. There is no pleasing Bob. Don’t be like Bob. No meat, fish or dairy products. If there’s an unlit BOB, replace them with Tomatoes. If there’s a lit BOB, replace them with Mushrooms." },
-            new Customer { Name = "Carlo", Description = "<b>Carlo</b> is Italian. Never put Pineapple on his pizza. Also, replace Pepperoni with Bell Peppers*. If there’s a lit CAR, replace the order with a Margherita.<br/><small>*) in Italy, Bell Peppers are called Peperoni (one p)</small>" },
-            new Customer { Name = "Clair", Description = "<b>Clair</b> is vegetarian*. Most of the time. As a general rule, remove all meat and fish. If there’s an unlit CLR, don’t remove Bacon. If there’s a lit CLR, double up on Bacon.<br/><small>*) she does eat cheese</small>" },
-            new Customer { Name = "Frank", Description = "<b>Frank</b> is diabetic. Remove BBQ Sauce or his foot will fall off. If there’s no FRK, replace with Tomatoes. If there’s a lit FRK, replace with Basil and Tomatoes." },
-            new Customer { Name = "Frédérique", Description = "<b>Frédérique</b> is allergic to Onions and Red onions. If there’s an unlit FRQ, replace with Cheddar. If there’s a lit FRQ, replace them with Italian sausage." },
-            new Customer { Name = "Ingrid", Description = "<b>Ingrid</b> is ... strange. Always replace Onions with Red onions and the other way around. If there’s an unlit IND, replace Bell peppers with Mushrooms. If there’s a lit IND, replace Mushrooms with Bell peppers." },
-            new Customer { Name = "Melissa", Description = "<b>Melissa</b> is 10 years old. No Jalapeño. If there’s no MSA, add one Pineapple. If there’s a lit MSA, add two Pineapple." },
-            new Customer { Name = "Natasha", Description = "<b>Natasha</b> likes to spice things up. If there is no NSA, add a Jalapeño. If there is a lit NSA, add two Jalapeño." },
-            new Customer { Name = "Sandy", Description = "<b>Sandy</b> is lactose intolerant. If there’s no SND, no cheese. If there is an SND, she has taken her lactose pills. If it's unlit, just leave the cheese. If it's lit, double up on cheese." },
-            new Customer { Name = "Sigmund", Description = "<b>Sigmund</b> should not get any Italian Sausage or Mussels. Otherwise he’ll try to convince you of having a castration complex. If there’s a lit SIG, no fish. If there’s an unlit SIG, no meat." },
-            new Customer { Name = "Tyrone", Description = "<b>Tyrone</b> hungry. Always fill up as much as you can with any meat or fish. If there is an unlit TRN, use anything to fill up. If there’s a lit TRN, ignore the order completely and just throw on it whatever you please, as much as you can."}
-        };
-
-        var manual = "";
-        foreach (var pizza in _pizzaRecipes)
-        {
-            manual += "<div><b>" + pizza.Value.Name + "</b><br/>"
-                + String.Join(", ", pizza.Value.Ingredients.Select(ingredient => _ingredientNames[ingredient]).ToArray()) + "</div>";
-        }
-        Debug.Log(manual);
-        manual = "";
-        foreach (var customer in _customers)
-        {
-            manual += "<div>" + customer.Description + "</div>";
-        }
-        Debug.Log(manual);
+        // Random order
+        _pizza = (Pizza)Rnd.Range(0, Enum.GetValues(typeof(Pizza)).Length);
+        _customer = (Customer)Rnd.Range(0, Enum.GetValues(typeof(Customer)).Length);
+        Order.transform.Find("Text").GetComponent<TextMesh>().text = _pizzaRecipes[_pizza].Name + "\n" + _customer;
 
         _itemsOnBelt = Enumerable.Repeat((Item)null, BeltNodes.Length).ToList();
         _itemsOnPlate = Enumerable.Repeat((Item)null, PlateNodes.Length).ToList();
@@ -137,6 +120,8 @@ public class PizzaModule : MonoBehaviour
             PlateNodes[i].OnInteract += delegate () { ReturnItem(j); return false; };
         }
 
+        Order.OnInteract += delegate () { CheckOrder(); return false; };
+
         StartCoroutine(MoveBelt());
     }
 
@@ -146,21 +131,20 @@ public class PizzaModule : MonoBehaviour
 
     private IEnumerator MoveBelt()
     {
+        bool maybeAddSomething = true;
+
         while (true)
         {
             yield return null;
 
-            // If the first node is empty, add something
-            if (_itemsOnBelt[0] == null)
+            // If the first node is empty, maybe add something
+            if (maybeAddSomething)
             {
-                var i = Rnd.Range(0, Ingredients.Length);
-                var item = new Item()
+                if (Rnd.Range(0, 3) != 0)
                 {
-                    Ingredient = (Ingredient)i,
-                    Instance = Instantiate(Ingredients[i], BeltNodes[0].transform),
-                };
-                _itemsOnBelt[0] = item;
-                //Debug.Log(item.Ingredient.ToString() + " incoming.");
+                    AddItemToBelt();
+                }
+                maybeAddSomething = false;
             }
 
             // If the nodes reached the starting point of the next node, reset the nodes and pass on all items to the next node
@@ -171,7 +155,6 @@ public class PizzaModule : MonoBehaviour
                     // Remove stuff at the end of the belt
                     if (i == BeltNodes.Length - 1 && _itemsOnBelt[i] is Item)
                     {
-                        //Debug.Log(_itemsOnBelt[i].Ingredient.ToString() + " leaving.");
                         Destroy(_itemsOnBelt[i].Instance.gameObject);
                     }
 
@@ -179,6 +162,7 @@ public class PizzaModule : MonoBehaviour
                     if (i == 0)
                     {
                         _itemsOnBelt[i] = null;
+                        maybeAddSomething = true;
                     }
                     else
                     {
@@ -204,6 +188,32 @@ public class PizzaModule : MonoBehaviour
                 );
             }
         }
+    }
+
+    private void AddItemToBelt()
+    {
+        // Refresh queued ingredients
+        if (_queuedIngredients.Count == 0)
+        {
+            var ingredients = new List<Ingredient>();
+            foreach (var pizzaRecipe in _pizzaRecipes)
+            {
+                foreach (var ingredient in pizzaRecipe.Value.Ingredients)
+                {
+                    ingredients.Add(ingredient);
+                }
+            }
+
+            ingredients.Shuffle();
+            _queuedIngredients = new Queue<Ingredient>(ingredients);
+        }
+
+        // Add item to belt
+        _itemsOnBelt[0] = new Item()
+        {
+            Ingredient = _queuedIngredients.Peek(),
+            Instance = Instantiate(Ingredients[(int)_queuedIngredients.Dequeue()], BeltNodes[0].transform),
+        };
     }
 
     private IEnumerator MoveItem(Item item, Vector3 from, Vector3 to, float duration)
@@ -257,6 +267,11 @@ public class PizzaModule : MonoBehaviour
         }
     }
 
+    private void CheckOrder()
+    {
+        // Check if ALL and NOTHING BUT the needed ingredients for the ordered pizza (_pizza) are on the plate (_itemsOnPlate)
+    }
+
     class Item
     {
         public Ingredient Ingredient { get; set; }
@@ -268,10 +283,20 @@ public class PizzaModule : MonoBehaviour
         public string Name { get; set; }
         public List<Ingredient> Ingredients { get; set; }
     }
+}
 
-    class Customer
+static class MyExtensions
+{
+    public static void Shuffle<T>(this IList<T> list)
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Rnd.Range(0, n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
     }
 }
